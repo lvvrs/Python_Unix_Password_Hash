e5814f36c308aefe6ece7125751e87c9a900d187
Vladimir Leonov <lvvrs@outlook.com>
2021-12-18 23:56:08 +0300

    Initialize work
    First version for test work with crypt module

---
+++ get_hash_unix_password.py
@@ -0,0 +1,29 @@
+"""
+This script generate hash for unix password.
+Available method's:
+1) MD5
+2) SHA-256
+3) SHA-512
+
+Author: Vladimir Leonov
+Release: 0.0.1
+Date: 18.12.2021
+"""
+
+
+import crypt
+
+password = "Pass0rd"
+
+hash_md5 = crypt.crypt(password, salt=crypt.METHOD_MD5)
+
+hash_sha256 = crypt.crypt(password, salt=crypt.METHOD_SHA256)
+
+hash_sha512 = crypt.crypt(password, salt=crypt.METHOD_SHA512)
+
+print("=====MD5=====")
+print(hash_md5)
+print("=====SHA-256=====")
+print(hash_sha256)
+print("=====SHA-512=====")
+print(hash_sha512)
