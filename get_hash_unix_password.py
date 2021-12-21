9949926733d9a0214d6ebc8d186695ebea4455b9
Vladimir Leonov <lvvrs@outlook.com>
2021-12-21 22:59:17 +0300

    version 2
    refactor code, script modify (using python function)

--- get_hash_unix_password.py
+++ get_hash_unix_password.py
@@ -6,8 +6,8 @@ Available method's:
 3) SHA-512
 
 Author: Vladimir Leonov
-Release: 0.0.1
-Date: 18.12.2021
+Release: 0.0.2
+Date: 21.12.2021
 Documentation: https://docs.python.org/3/library/crypt.html
 Lib Repository: https://github.com/python/cpython/blob/3.10/Lib/crypt.py
 """
@@ -15,13 +15,28 @@ Lib Repository: https://github.com/python/cpython/blob/3.10/Lib/crypt.py
 
 import crypt
 
-password = "Pass0rd"
 
-hash_md5 = crypt.crypt(password, salt=crypt.METHOD_MD5)
+def get_pass_hash(password, method):
+    if method == "md5":
+        pass_hash = crypt.crypt(password, salt=crypt.METHOD_MD5)
+    elif method == "sha256":
+        pass_hash = crypt.crypt(password, salt=crypt.METHOD_SHA256)
+    elif method == "sha512":
+        pass_hash = crypt.crypt(password, salt=crypt.METHOD_SHA512)
+    else:
+        pass_hash = "Error, Bad crypt method!"
+    return pass_hash
 
-hash_sha256 = crypt.crypt(password, salt=crypt.METHOD_SHA256)
 
-hash_sha512 = crypt.crypt(password, salt=crypt.METHOD_SHA512)
+pass_for_crypt = "Pass0rd"
+
+hash_md5 = get_pass_hash(pass_for_crypt, "md5")
+
+hash_sha256 = get_pass_hash(pass_for_crypt, "sha256")
+
+hash_sha512 = get_pass_hash(pass_for_crypt, "sha512")
+
+bad_hash = get_pass_hash(pass_for_crypt, "sha513")
 
 print("=====MD5=====")
 print(hash_md5)
@@ -29,3 +44,5 @@ print("=====SHA-256=====")
 print(hash_sha256)
 print("=====SHA-512=====")
 print(hash_sha512)
+print("=====Bad_Hash=====")
+print(bad_hash)
